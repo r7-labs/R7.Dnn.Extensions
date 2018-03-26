8a13cd1a39b2f3d54d62cd3695f10c29a15a6fb4
roman-yagodin <roman.yagodin@gmail.com>
2018-03-26 11:17:48 +0300

    Close GH-14 Add IsNullOrEmpty() extension methods for the IEnumerable<T> and ICollection<T>

---
+++ R7.Dnn.Extensions.Tests/Collections/EnumerableExtensionsTests.cs
@@ -0,0 +1,56 @@
+﻿//
+//  EnumerableExtensionsTests.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2018 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Collections.Generic;
+using R7.Dnn.Extensions.Collections;
+using Xunit;
+
+namespace R7.Dnn.Extensions.Tests.Collections
+{
+    public class EnumerableExtensionsTests
+    {
+        [Fact]
+        public void IsNullOrEmptyTest  ()
+        {
+            var emptyEnumerable = GetEmptyEnumerable ();
+            var emptyCollection = new List<object> ();
+
+            Assert.True (emptyEnumerable.IsNullOrEmpty ());
+            Assert.True (emptyCollection.IsNullOrEmpty ());
+
+            var enumerable = GetEnumerable ();
+            var collection = new List<object> { new object () };
+
+            Assert.False (enumerable.IsNullOrEmpty ());
+            Assert.False (collection.IsNullOrEmpty ());
+        }
+
+        IEnumerable<object> GetEnumerable ()
+        {
+            yield return new object ();
+        }
+
+        IEnumerable<object> GetEmptyEnumerable ()
+        {
+            yield break;
+        }
+    }
+}
---
+++ R7.Dnn.Extensions/Collections/EnumerableExtensions.cs
@@ -0,0 +1,61 @@
+﻿//
+//  EnumerableExtensions.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2018 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace R7.Dnn.Extensions.Collections
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Determines whether the elemerable is null or contains no elements.
+        /// </summary>
+        /// <typeparam name="T">The type of IEnumerable elements.</typeparam>
+        /// <param name="enumerable">The enumerable, which may be null or empty.</param>
+        /// <returns><c>true</c> if the IEnumerable is null or empty; otherwise, <c>false</c>.</returns>
+        public static bool IsNullOrEmpty<T> (this IEnumerable<T> enumerable)
+        {
+            if (enumerable == null) {
+                return true;
+            }
+
+            // if this is a collection, use the O(1) Count property
+            var collection = enumerable as ICollection<T>;
+            if (collection != null) {
+                return collection.Count == 0;
+            }
+
+            return !enumerable.Any ();
+        }
+
+        /// <summary>
+        /// Determines whether the collection is null or contains no elements.
+        /// </summary>
+        /// <typeparam name="T">The type of ICollection elements.</typeparam>
+        /// <param name="collection">The collection, which may be null or empty.</param>
+        /// <returns><c>true</c> if the ICollection is null or empty; otherwise, <c>false</c>.</returns>
+        public static bool IsNullOrEmpty<T> (this ICollection<T> collection)
+        {
+            return (collection == null) || (collection.Count == 0);
+        }
+    }
+}
