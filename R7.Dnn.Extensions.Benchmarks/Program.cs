9b9f6496f84cca92ae152d9f5338ab04338887bf
roman-yagodin <roman.yagodin@gmail.com>
2018-03-27 12:21:57 +0300

    Add benchmark for FormatHelper methods

---
+++ R7.Dnn.Extensions.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using R7.Dnn.Extensions.Benchmarks.Text;
+
+namespace R7.Dnn.Extensions.Benchmarks
+{
+    class Program
+    {
+        public static void Main (string [] args)
+        {
+            var summary = BenchmarkRunner.Run<FormatHelperBenchmark> ();
+        }
+    }
+}
---
+++ R7.Dnn.Extensions.Benchmarks/Text/FormatHelperBenchmark.cs
@@ -0,0 +1,59 @@
+﻿using R7.Dnn.Extensions.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace R7.Dnn.Extensions.Benchmarks.Text
+{
+    [MemoryDiagnoser]
+    public class FormatHelperBenchmark
+    {
+        readonly string [] strData = {
+            "string 1",
+            "string 2",
+            "string 3",
+            "string 4",
+            "string 5",
+            "string 6",
+            "string 7",
+            "string 8",
+            "string 9",
+            "string 10"
+        };
+
+        readonly object [] objData = {
+            "string 1",
+            "string 2",
+            "string 3",
+            "string 4",
+            "string 5",
+            "string 6",
+            "string 7",
+            "string 8",
+            "string 9",
+            "string 10"
+        };
+
+        [Benchmark]
+        public string FormatList_Strings ()
+        {
+            return FormatHelper.FormatList (", ", strData);
+        }
+
+        [Benchmark]
+        public string FormatList_Objects ()
+        {
+            return FormatHelper.FormatList (", ", objData);
+        }
+
+        [Benchmark]
+        public string JoinNotNullOrEmpty_Strings ()
+        {
+            return FormatHelper.JoinNotNullOrEmpty (", ", strData);
+        }
+
+        [Benchmark]
+        public string JoinNotNullOrEmpty_Objects ()
+        {
+            return FormatHelper.JoinNotNullOrEmpty (", ", objData);
+        }
+    }
+}
