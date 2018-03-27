bad38ddf9c588549b7d0b5ed2b60f0f6769b0814
roman-yagodin <roman.yagodin@gmail.com>
2018-03-27 12:24:08 +0300

    Add license and license headers

--- R7.Dnn.Extensions.Benchmarks/Program.cs
+++ R7.Dnn.Extensions.Benchmarks/Program.cs
@@ -1,4 +1,25 @@
-﻿using BenchmarkDotNet.Running;
+﻿//
+//  Program.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2018 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using BenchmarkDotNet.Running;
 using R7.Dnn.Extensions.Benchmarks.Text;
 
 namespace R7.Dnn.Extensions.Benchmarks
