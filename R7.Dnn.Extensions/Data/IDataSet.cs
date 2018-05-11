ef38d1e95571f4cf4dfa0ccd2fc82eda6ba768b1
roman-yagodin <roman.yagodin@gmail.com>
2018-05-11 13:33:15 +0300

    Port ORM abstractions from R7.University, introduce IDataSet

---
+++ R7.Dnn.Extensions/Data/IDataSet.cs
@@ -0,0 +1,42 @@
+﻿//
+//  IDataSet.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2018 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Linq;
+
+namespace R7.Dnn.Extensions.Data
+{
+    public interface IDataSet<TEntity> where TEntity: class
+    {
+        IQueryable<TEntity> Query ();
+
+        IQueryable<TEntity> FromSql (string queryName, params object [] parameters);
+
+        TEntity Find<TKey> (TKey key);
+
+        bool Exists (TEntity entity);
+
+        void Add (TEntity entity);
+
+        void Attach (TEntity entity);
+
+        void Remove (TEntity entity);
+    }
+}
---
+++ R7.Dnn.Extensions/Models/IModelContext.cs
@@ -0,0 +1,54 @@
+﻿//
+//  IModelContext.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2018 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace R7.Dnn.Extensions.Models
+{
+    public interface IModelContext: IDisposable
+    {
+        IQueryable<TEntity> Query<TEntity> () where TEntity : class;
+
+        IQueryable<TEntity> QueryOne<TEntity> (Expression<Func<TEntity, bool>> keySelector) where TEntity : class;
+
+        IQueryable<TEntity> Query<TEntity> (string sql, params object [] parameters) where TEntity : class;
+
+        TEntity Get<TEntity, TKey> (TKey key) where TEntity : class;
+
+        bool Exists<TEntity> (TEntity entity) where TEntity : class;
+
+        void Add<TEntity> (TEntity entity) where TEntity : class;
+
+        void Update<TEntity> (TEntity entity) where TEntity : class;
+
+        void UpdateExternal<TEntity> (TEntity entity) where TEntity : class;
+
+        void Remove<TEntity> (TEntity entity) where TEntity : class;
+
+        void RemoveExternal<TEntity> (TEntity entity) where TEntity : class;
+
+        bool SaveChanges (bool dispose = true);
+
+        ITransaction BeginTransaction ();
+    }
+}
