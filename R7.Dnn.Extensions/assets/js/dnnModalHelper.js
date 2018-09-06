e44c60660d307cc3c5c719ac3ce43e003aec2e1b
roman-yagodin <roman.yagodin@gmail.com>
2018-09-06 11:31:44 +0300

    GH-15 dnnModalHelper script

---
+++ R7.Dnn.Extensions/assets/js/dnnModalHelper.js
@@ -0,0 +1,20 @@
+﻿(function ($, window) {
+  window.dnnModalHelper = {
+    getPopup: function (_window) {
+      return _window.jQuery("#iPopUp");
+    },
+    disableRefresh: function (popup) {
+      if (popup.dialog("option", "refresh")) {
+        popup.dialog("option", "refresh", false);
+        window.dnnModalHelper.closingUrl = popup.dialog("option", "closingUrl");
+        popup.dialog("option", "close", null);
+      }
+    },
+    enableRefresh: function (popup, closingUrl) {
+      popup.dialog("option", "refresh", true);
+      popup.dialog("option", "close", function() {
+        window.dnnModal.closePopUp(true, window.dnnModalHelper.closingUrl);
+      });
+    }
+  }
+}) (jQuery, window);
