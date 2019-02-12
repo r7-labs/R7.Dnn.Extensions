8ae8b03737b4365f6aaf838d8e6439e12a7768cd
roman-yagodin <roman.yagodin@gmail.com>
2019-02-12 15:55:46 +0400

    Bump version

--- R7.Dnn.Extensions/Properties/AssemblyInfo.cs
+++ R7.Dnn.Extensions/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("0.13.0.*")]
-[assembly: AssemblyInformationalVersion ("0.13-rc.1")]
+[assembly: AssemblyInformationalVersion ("0.13.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
